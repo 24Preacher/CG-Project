016106312d805fb8ddbd60e55f0878dfb1df12ae
DavideMatos14 <g14dsm@gmail.com>
2020-04-14 22:43:06 +0100

    guiao7

--- guiao6/code/main.cpp
+++ guiao6/code/main.cpp
@@ -20,23 +20,129 @@ float height=2.0f;
 float x = 0.0f;
 float z = 0.0f;
 
-float s = 0.0;
 
-float camX = 00, camY = 30, camZ = 40;
-int startX, startY, tracking = 0;
+float px = 0, py = 0, pz = 0;
 
-int alpha = 0, beta = 45, r = 50;
-
+float alfaview = 0 , betaview = 0, step = 2.0;
+float dx,dy,dz;
+float eyev = 2.0;
 unsigned int t, tw, th, m;
 unsigned char *imageData;
 float *vertexB;
 GLuint  buffers[1];
+int seed = 1;
+int mode = GL_LINE;
 
-
-float h(int i, int j){
-    return (*(imageData + j*tw + i));
+float normA (float x ,float y, float z){
+    return sqrt(x*x + y*y + z*z);
 }
 
+void normaliza (float* x ,float* y, float* z){
+    float xx= *x, yy = *y, zz= *z;
+    float norma = normA (xx,yy,zz);
+    *x = xx/norma;
+    *y = yy/norma;
+    *z = zz/norma;
+}
+
+float h(int i, int j){
+    return (float) (*(imageData + j*tw + i));
+}
+
+
+float hf(float x, float z){
+  int x1 = floor(x);
+  int x2 = x1 + 1;
+  int z1 = floor(z);
+  int z2 = z1 + 1;
+  float px,pz;
+  float fx = x-x1;
+  float fz = z-z1;
+  float h_x1_z = h(x1,z1) * (1-fz) + h(x1,z2) * fz;
+  float h_x2_z = h(x2,z1) * (1-fz) + h(x2,z2) * fz;
+  return (h_x1_z * (1 - fx) + h_x2_z * fx);
+
+}
+
+void arvores(int n){
+
+    float x,z;
+    srand(seed);
+    int i;
+    float posX[n];
+    float posZ[n];
+    for(i = 0; i<n; i++){
+
+      posX[i] = ((float)rand()/(float)(RAND_MAX))*200.0 - 100.0;
+      posZ[i] = ((float)rand()/(float)(RAND_MAX))*200.0 - 100.0;
+        if (normA(posX[i],0,posZ[i]) < 50)
+        i--;
+
+
+    }
+
+    for(i = 0; i<n; i++){
+
+        x = posX[i];
+        z = posZ[i];
+
+
+        glPushMatrix();
+        glTranslatef(x, hf(x+128,z+128), z);
+        glRotatef(-90,1,0,0);
+        glColor3f(0.647059,0.164706,0.164706);
+        glutSolidCone(1, 3, 4, 4);
+        glTranslatef(0,0,2.5);
+        glColor3f(0.137255,0.556863,0.137255);
+        glutSolidCone(3, 5, 10, 10);
+        glPopMatrix();
+
+
+
+    }
+}
+void controlCoordenadas(){
+	if (px < -128)
+		px = -128;
+	if (px > 128)
+		px = 128;
+	if (pz < -128)
+		pz = -128;
+	if (pz > 128)
+		pz = 128;
+}
+
+void drawteepots(int number, float radius){
+  float alfa = 0;
+  float angleStep = 2*M_PI/number;
+  float px, pz;
+  int i=0;
+
+
+    for (i=0; i<number; i++){
+      px = radius * sin(alfa);
+      pz = radius * cos(alfa);
+      glPushMatrix();
+      glTranslatef(px,hf(px+128,pz+128)+0.75,pz);
+      glRotatef(alfa*180/M_PI -90 ,0,1,0);
+      glutSolidTeapot(1);
+      alfa += angleStep;
+      glPopMatrix();
+
+    }
+}
+
+void direcao(){
+  float viewX, viewY, viewZ;
+  viewX = cos(betaview)*sin(alfaview);
+  viewY = sin(betaview);
+  viewZ = cos(betaview)*cos(alfaview);
+
+  normaliza (&viewX,&viewY,&viewZ);
+  dx = viewX;
+  dy = viewY;
+  dz = viewZ;
+}
 
 
 void changeSize(int w, int h) {
@@ -46,13 +152,13 @@ void changeSize(int w, int h) {
 	if(h == 0)
 		h = 1;
 
-	// compute window's aspect ratio 
+	// compute window's aspect ratio
 	float ratio = w * 1.0 / h;
 
 	// Reset the coordinate system before modifying
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	
+
 	// Set the viewport to be the entire window
     glViewport(0, 0, w, h);
 
@@ -70,106 +176,148 @@ void changeSize(int w, int h) {
 
 
 void renderScene(void) {
-    int i, j;
-	float pos[4] = {-1.0, 1.0, 1.0, 0.0};
+    int i;
+    direcao();
 
 	glClearColor(0.0f,0.0f,0.0f,0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glLoadIdentity();
-	gluLookAt(camX, camY, camZ, 
-		      0.0,0.0,0.0,
-			  0.0f,1.0f,0.0f);
-
+  gluLookAt(px,  hf(px+128,pz+128)+ eyev, pz,
+            px + dx, hf(px+128,pz+128) + eyev + dy,pz + dz,
+            0.0f,1.0f,0.0f);
 
 	// just so that it renders something before the terrain is built
 	// to erase when the terrain is ready
+  glPolygonMode(GL_FRONT_AND_BACK,mode);
     glBindBuffer(GL_ARRAY_BUFFER,buffers[0]);
+    glColor3f(0.5,1,0.5);
+
 	glVertexPointer(3,GL_FLOAT,0,0);
 	for(i = 0; i < th -1; i++){
-	    j = (m/3) / (th - 1);
 	    glDrawArrays(GL_TRIANGLE_STRIP, i*tw*2, tw*2);
 	}
-    s++;
-	//glutWireTeapot(2.0);
+
+
+
+    arvores(100);
+
+    glColor3f(0,0,1);
+    drawteepots(6,15);
+
+    glColor3f(1,0,0);
+    drawteepots(24,35);
+
+     glPushMatrix();
+
+     glTranslatef(0,hf(128,128),0);
+     glColor3f(0.737255,0.560784,0.560784);
+     glutSolidTorus(1, 3, 10, 10);
+     glPopMatrix();
 
 // End of frame
 	glutSwapBuffers();
 }
 
 
+void help() {
 
-void processKeys(unsigned char key, int xx, int yy) {
-
-// put code to process regular keys in here
+        printf ( " ┌-------------------------------------------------HELP---------------------------------------------┐\n");
+        printf ( " |                                                                                                  |\n");
+        printf ( " |                                                                                                  |\n");
+        printf ( " |                                                                                                  |\n");
+        printf ( " |      Controls :                                                                                  |\n");
+        printf ( " |         move forward: w                                                                          |\n");
+        printf ( " |         move backwards: s                                                                        |\n");
+        printf ( " |         move right: d                                                                            |\n");
+        printf ( " |         move left : a                                                                            |\n");
+        printf ( " |         look left: LEFT KEY                                                                      |\n");
+        printf ( " |         look right: RIGHT KEY                                                                    |\n");
+        printf ( " |         look up: UP KEY                                                                          |\n");
+        printf ( " |         look down: DOWN KEY                                                                      |\n");
+        printf ( " |         switch drawing mode: m                                                                   |\n");
+        printf ( " |         switch tree seed: n                                                                      |\n");
+        printf ( " |                                                                                                  |\n");
+        printf ( " └--------------------------------------------------------------------------------------------------┘\n");
 }
 
 
 
-void processMouseButtons(int button, int state, int xx, int yy) {
-	
-	if (state == GLUT_DOWN)  {
-		startX = xx;
-		startY = yy;
-		if (button == GLUT_LEFT_BUTTON)
-			tracking = 1;
-		else if (button == GLUT_RIGHT_BUTTON)
-			tracking = 2;
-		else
-			tracking = 0;
-	}
-	else if (state == GLUT_UP) {
-		if (tracking == 1) {
-			alpha += (xx - startX);
-			beta += (yy - startY);
-		}
-		else if (tracking == 2) {
-			
-			r -= yy - startY;
-			if (r < 3)
-				r = 3.0;
-		}
-		tracking = 0;
-	}
+void processKeys(unsigned char c, int xx, int yy) {
+
+    switch (c) {
+        case 'd':
+            alfaview -= M_PI/2;
+            direcao();
+            px += step*dx;
+            pz += step*dz;
+            alfaview += M_PI/2;
+            controlCoordenadas();
+            break;
+        case 'a':
+            alfaview += M_PI/2;
+            direcao();
+            px +=step*dx;
+            pz += step*dz;
+            alfaview -= M_PI/2;
+            controlCoordenadas();
+            break;
+        case 'w':
+            px += step*dx;
+            pz += step*dz;
+            controlCoordenadas();
+            break;
+        case 's':
+            px -= step*dx;
+            pz -= step*dz;
+            controlCoordenadas();
+            break;
+        case 'm':
+            if(mode == GL_LINE){
+                mode = GL_POINT;
+                break;
+            }else if(mode == GL_POINT){
+                mode = GL_FILL;
+                break;
+            }else if(mode == GL_FILL){
+                mode = GL_LINE;
+                break;
+            }
+            break;
+        case 'n':
+            seed = rand();
+            break;
+        default:
+            break;
+    }
+    glutPostRedisplay();
 }
 
+void processSpecialKeys(int key, int xx, int yy) {
 
-void processMouseMotion(int xx, int yy) {
+    switch (key) {
+        case GLUT_KEY_UP :
+            betaview += M_PI/64;
+            if (betaview > M_PI/3)
+                betaview = M_PI / 3;
+            break;
+        case GLUT_KEY_DOWN:
+            betaview -= M_PI/64;
+            if (betaview < -M_PI/3)
+                betaview = -M_PI / 3;
+            break;
+        case GLUT_KEY_RIGHT:
+            alfaview -= M_PI/64;
+            break;
+        case GLUT_KEY_LEFT:
+            alfaview += M_PI/64;
+            break;
+        default:
+            break;
+    }
 
-	int deltaX, deltaY;
-	int alphaAux, betaAux;
-	int rAux;
+    glutPostRedisplay();
 
-	if (!tracking)
-		return;
-
-	deltaX = xx - startX;
-	deltaY = yy - startY;
-
-	if (tracking == 1) {
-
-
-		alphaAux = alpha + deltaX;
-		betaAux = beta + deltaY;
-
-		if (betaAux > 85.0)
-			betaAux = 85.0;
-		else if (betaAux < -85.0)
-			betaAux = -85.0;
-
-		rAux = r;
-	}
-	else if (tracking == 2) {
-
-		alphaAux = alpha;
-		betaAux = beta;
-		rAux = r - deltaY;
-		if (rAux < 3)
-			rAux = 3;
-	}
-	camX = rAux * sin(alphaAux * 3.14 / 180.0) * cos(betaAux * 3.14 / 180.0);
-	camZ = rAux * cos(alphaAux * 3.14 / 180.0) * cos(betaAux * 3.14 / 180.0);
-	camY = rAux * 							     sin(betaAux * 3.14 / 180.0);
 }
 
 
@@ -177,6 +325,7 @@ void processMouseMotion(int xx, int yy) {
 void plano(){
     float d = 1.0;
     int i, j;
+
 
     float x_neg = -(tw/2.0);
     float z_neg = -(th/2.0);
@@ -215,7 +364,7 @@ void init() {
     th = ilGetInteger(IL_IMAGE_HEIGHT);
 
     imageData = ilGetData();
-    //plano();
+    plano();
 // 	Build the vertex arrays
     glEnableClientState(GL_VERTEX_ARRAY);
     glGenBuffers(1,buffers);
@@ -230,29 +379,28 @@ void init() {
 
 int main(int argc, char **argv) {
 
+  help();
 // init GLUT and the window
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
 	glutInitWindowPosition(100,100);
-	glutInitWindowSize(320,320);
+	glutInitWindowSize(800,600);
 	glutCreateWindow("CG@DI-UM");
-		
+	glewInit();
 
-// Required callback registry 
+// Required callback registry
 	glutDisplayFunc(renderScene);
 	glutIdleFunc(renderScene);
 	glutReshapeFunc(changeSize);
 
 // Callback registration for keyboard processing
 	glutKeyboardFunc(processKeys);
-	glutMouseFunc(processMouseButtons);
-	glutMotionFunc(processMouseMotion);
+  glutSpecialFunc(processSpecialKeys);
 
-	init();	
+	init();
 
 // enter GLUT's main cycle
 	glutMainLoop();
-	
+
 	return 0;
 }
-
